775f6e35d73f2c21034cb3e1dafa3932717651c4
Taras-Melnychenko <tarasmelnychenko@gmail.com>
2016-10-09 19:47:41 +0300

    HomeWork6

---
+++ src/Module6/Main.java
@@ -0,0 +1,29 @@
+package Module6;
+
+
+public class Main {
+    public static void main(String[] args) {
+        User[] users = new User[5];
+        User user1 = new User(12, "Taras", "Val", 300, 1000);
+        users[0] = user1;
+        User user2 = new User(12, "Taras", "Val", 300, 1000);/*(13, "Oleg", "Sal", 250, 750);*/
+        users[1] = user2;
+        User user3 = new User(14, "Kale", "Mel", 400, 400);
+        users[2] = user3;
+        User user4 = new User(15, "Ilona", "Gal", 200, 450);
+        users[3] = user4;
+        User user5 = null;
+        users[4] = user5;
+
+ //       User[]  resSout = UserUtills.deleteEmptyUsers(users);
+ //       long[] resSout = UserUtills.getUsersId(users);
+ //       User[] resSout = UserUtills.paySalaryToUsers(users);
+ //       User[] resSout = UserUtills.usersWithContitionalBalance(users, 1000);
+ //       User[] resSout = UserUtills.uniqueUsers(users);
+        User[] resSout = UserUtills.deleteEmptyUsers(users);
+        for (int i = 0; i < resSout.length; i++){
+            System.out.println(resSout[i]);
+        }
+    }
+}
+
--- src/Module6/UserUtills.java
+++ src/Module6/UserUtills.java
@@ -3,22 +3,73 @@ package Module6;
 
 public class UserUtills {
     static User[] uniqueUsers(User[] users){
-        return users;
+        for (int i = 0; i < users.length; i++){
+            for (int a = i+1; a < users.length; a++){
+                if (users[i].equals(users[a])&&users[i]!=null){
+                    users[a] = null;
+                }
+            }
+        }
+        return deleteEmptyUsers(users);
     }
     //users are equal when all their field are equal
     static User[] usersWithContitionalBalance(User[] users, int balance){
-        return users;
+        int i = 0;
+        int counter = 0;
+        while (i < users.length){
+            if (users[i].getBalance() == balance){
+                counter++;
+            }
+            i++;
+        }
+        User[] usersWithContitionalBalance = new User[counter];
+        int j = 0;
+        int c = 0;
+        while (j < users.length){
+            if (users[j].getBalance() == balance){
+                usersWithContitionalBalance[c] = users[j];
+                c++;
+            }
+            j++;
+        }
+        return usersWithContitionalBalance;
     }
 //user’s balance == balance
 
     static User[] paySalaryToUsers(User[] users){
+        int i = 0;
+        while(i < users.length) {
+            users[i].setBalance(users[i].getSalary()+users[i].getBalance());
+            i++;
+        }
         return users;
     }
- //   static long[] getUsersId(User[] users){
- //       long[] getUsersId = new long[users.length];
- //       return getUsersId;
- //   }
-    User[] deleteEmptyUsers(User[] users){
-        return users;
+    static long[] getUsersId(User[] users){
+        long[] getUsersId = new long[users.length];
+        int i = 0;
+        while(i < users.length){
+            getUsersId[i] = users[i].getId();
+            i++;
+        }
+        return getUsersId;
+    }
+    static User[] deleteEmptyUsers(User[] users){
+        int counter = 0;
+        int i = 0;
+        while (i < users.length){
+            if (users[i] != null) counter++;
+            i++;
+        }
+        int j = 0;
+        int c = 0;
+        User[] deleteEmptyUsers = new User[counter];
+        while (j < users.length){
+            if (users[j] != null) {
+                deleteEmptyUsers[c] = users[j];
+                c++;
+            }
+            j++;
+        }
+        return deleteEmptyUsers;
     }
 }
